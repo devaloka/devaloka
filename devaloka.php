6a5fad2bd9d95c9a2f57c51dc78ef5f331b09315
Whizark <devaloka@whizark.com>
2015-12-27 00:07:56 +0900

    fix: incorrect version number in plugin header comment

--- devaloka.php
+++ devaloka.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Devaloka
 Description: A WordPress plugin brings DI Container, Event Dispatcher to WordPress
-Version: 0.5.0
+Version: 0.5.1
 Author: Whizark
 Author URI: http://whizark.com
 License: GPL-2.0+
