f0a0c82f827850bdf47e7285b368a536b1a4f2f6
Whizark <devaloka@whizark.com>
2016-01-16 04:56:04 +0900

    feat(Sidebar): introduce AbstractSidebar class

---
+++ src/Component/Sidebar/AbstractSidebar.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Abstract Sidebar
+ *
+ * @author Whizark <devaloka@whizark.com>
+ * @see http://whizark.com
+ * @copyright Copyright (C) 2015 Whizark.
+ * @license MIT
+ */
+
+namespace Devaloka\Component\Sidebar;
+
+/**
+ * Class AbstractSidebar
+ *
+ * @package Devaloka\Component\Sidebar
+ */
+abstract class AbstractSidebar implements SidebarInterface
+{
+    use SidebarTrait;
+}
