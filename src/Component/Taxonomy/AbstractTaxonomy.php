2e27c47df6175008bd0c2417ebddee0a0af0ad72
Whizark <devaloka@whizark.com>
2016-01-16 11:30:35 +0900

    feat(Taxonomy): introduce AbstractTaxonomy class

---
+++ src/Component/Taxonomy/AbstractTaxonomy.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Abstract Taxonomy
+ *
+ * @author Whizark <devaloka@whizark.com>
+ * @see http://whizark.com
+ * @copyright Copyright (C) 2015 Whizark.
+ * @license MIT
+ */
+
+namespace Devaloka\Component\Taxonomy;
+
+/**
+ * Class AbstractTaxonomy
+ *
+ * @package Devaloka\Component\Taxonomy
+ */
+abstract class AbstractTaxonomy implements TaxonomyInterface
+{
+    use TaxonomyTrait;
+}
